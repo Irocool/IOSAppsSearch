eddf2129dea1369a1ec479751cfb3d0fe25965df
Irocool <ikuligina@gmail.com>
2022-02-23 23:06:49 +0300

    Add project

---
+++ iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailDescriptionView.swift
@@ -0,0 +1,11 @@
+//
+//  AppDetailDescriptionView.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Irina Kuligina on 23.02.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import UIKit
+
+
